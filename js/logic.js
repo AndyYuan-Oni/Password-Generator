208ceafd11d515095b699ca4417057e6ee43e09f
Andy <andyyuan2017@gmail.com>
2019-11-20 18:40:19 +1100

    pseudoCoding&randomFunctions

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,26 @@
-fucntion randomSpecial() {
+//Generate random special
+function ranSpec() {
     var sStr = "!\"§$%&/()=?\u{20ac}";
-    return sStr[sStr.length * Math.random()];
+    return sStr[Math.floor(sStr.length * Math.random())];
+};
+console.log(ranSpec());
+//Generate random lowercase character
+function ranChar() {
+    var cStr = "abcdefghijklmnopqrstuvwxyz";
+    return cStr[Math.floor(cStr.length * Math.random())];
 }
-
-console.log(randomSpecial());
+console.log(ranChar());
+//generate random uppercase character
+function ranCChar() {
+    var CaStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return CaStr[Math.floor(CaStr.length * Math.random())];
+}
+console.log(ranCChar());
+//generate random number
+function ranNum() {
+    var numStr = "1234567890";
+    return numStr[Math.floor(numStr.length * Math.random())];
+}
+console.log(ranNum());
+//generate first several characters with all of the needed characters
+//generate random function calls for random times
